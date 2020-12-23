e360e074f05e88bb9b8e0096979cf2fd8b85c754
Noor Ahmed <noora@resortsuite.com>
2020-12-23 18:43:30 -0500

    hh

--- spa/src/index.js
+++ spa/src/index.js
@@ -9,7 +9,7 @@ const start = async () => {
   // }
 
   app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!!!!!!! spa');
+    console.log('Listening on port 3000!!!!!!!!!!!! spa');
   });
 };
 
